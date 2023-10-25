e5a30a7671c1a62f9a75e6ca7f889f8b042ef7a0
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-10-25 14:36:08 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,31 @@
+# # # # nint = int(input('Digite o número: '))
+# # # # if nint % 2 == 0 :
+# # # #     print(f'O número é par')
+# # # # else :
+# # # #     print(f'O número é impar')
+
+# # # km = float(input('Digite a distancia da viagem: '))
+# # # if km <= 200 :
+# # #     passagem = 0.50 * km
+# # # else :
+# # #     passagem = 0.45 * km
+# # # print(f'O preço da passagem é: R$ {passagem}')
+
+# # nint1 = int(input('Digite o primeiro valor: '))
+# # nint2 = int(input('Digite o segundo valor: '))
+# # nint3 = int(input('Digite o terceiro valor: '))
+# # if nint1 > nint2 and nint3 :
+# #     print(f'{nint1} é maior que {nint2} e {nint3}')
+# # if nint2 > nint3 and nint1 :
+# #     print(f'{nint2} é maior que {nint1} e {nint3}')
+# # else :
+# #     print(f'{nint3} é maior que {nint1} e {nint2}')
+
+# velocidade = float(input('Qual era a velocidade do veículo: '))
+
+# if velocidade > 80 :
+#     print(f'Este veículo foi multado poís estava a {velocidade}')
+#     multa = (velocidade - 80) * 7
+#     print(f'Você deve pagar uma multa de R${multa}')
+# else :
+#     print(f'Este veículo estava nos limites de velocidade.')
